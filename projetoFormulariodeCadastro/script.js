f37c810f0de322a0a50635c46481c00a34a93916
Inaldo Macena <96094397+inaldomacena@users.noreply.github.com>
2022-06-25 00:14:12 -0300

    atualização

--- projetoFormulariodeCadastro/script.js
+++ projetoFormulariodeCadastro/script.js
@@ -1,8 +1,27 @@
-function mostrarOcultarSenha(){
+/*function mostrarOcultarSenha(){
     var senha = document.getElementById("senha");
     if (senha.type=="password"){
         senha.type="texto";
     }else{
         senha.type="password";
     }
+}*/
+
+/*const form = document.getElementById('form');
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const telefone = document.getElementById('telefone');*/
+
+let botao = document.querySelector('.fa-eye')
+botao.addEventListener (click,()=>{
+    let inputSenha = document.querySelector('#senha')
+    if(inputSenha.getAttribute('type')== 'passaword'){
+        inputSenha.setAttribute('type', text)
+} else{
+    inputSenha.setAttribute('type', 'password')
 }
+
+
+}
+
+  
